243a7b67ec9bff13e371458df30ecd6f18b7f0e0
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-09-07 18:55:52 +0200

    Implement a basic size fixed Queue

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,38 @@
+var Queue = (function() {
+  function Queue(size) {
+    this.size = size;
+    this.items = [];
+    this.version = 0;
+  }
+
+  Queue.prototype.push = function(item) {
+    this.items.push([this.version, item]);
+    this.version += 1;
+
+    if (this.items.length > this.size)
+      this.items.shift();
+  };
+
+  Queue.prototype.toTrQueue = function(trackid) {
+    var trQueue = [];
+    var include = false;
+    this.items.forEach(function(item) {
+      var version = item[0];
+      var op = item[1];
+
+      if (version === trackid)
+        include = true;
+
+      if (include)
+        trQueue.push(op);
+    });
+
+    return trQueue;
+  };
+
+  return Queue;
+}());
+
 var Scratchpad = (function() {
 
   function Scratchpad() {
--- test/scratchpad_tests.js
+++ test/scratchpad_tests.js
@@ -27,3 +27,58 @@ describe("Scratchpad", function() {
     });
   });
 });
+
+describe("Queue", function() {
+  describe("constructor", function() {
+    it("should have a size property", function() {
+      var queue = new Queue(10);
+      expect(queue.size).to.equal(10);
+    });
+
+    it("should have an array of items", function() {
+      var queue = new Queue(10);
+      expect(queue.items).to.be.an(Array);
+    });
+
+    it("should have a version property", function() {
+      var queue = new Queue(10);
+      expect(queue.version).to.equal(0);
+    });
+  });
+
+  describe("#push", function() {
+    it("should add an item to the queue", function() {
+      var queue = new Queue(10);
+      queue.push("item1");
+      queue.push("item2");
+      expect(queue.items.length).to.equal(2);
+    });
+
+    it("should keep a fixed number of items", function() {
+      var queue = new Queue(3);
+      queue.push("item1");
+      queue.push("item2");
+      queue.push("item3");
+      queue.push("item4");
+      expect(queue.items.length).to.equal(3);
+    });
+  });
+
+  describe("#toTrQueue", function() {
+    it("should return an array of items", function() {
+      var queue = new Queue(10);
+      queue.push("item1");
+      queue.push("item2");
+      queue.push("item3");
+      expect(queue.toTrQueue(0)).to.eql(["item1", "item2", "item3"]);
+    });
+
+    it("should return a filtered array of items", function() {
+      var queue = new Queue(10);
+      queue.push("item1");
+      queue.push("item2");
+      queue.push("item3");
+      expect(queue.toTrQueue(2)).to.eql(["item3"]);
+    });
+  });
+});
